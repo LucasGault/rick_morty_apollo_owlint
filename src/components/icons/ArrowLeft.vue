e78ad0abb958f2d86fbbe9405939d0c67450ffeb
Lucas Gault <lucasgault12@gmail.com>
2022-03-24 14:22:47 +0100

    add Icons

---
+++ src/components/icons/ArrowLeft.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg
+    :width="width"
+    :height="height"
+    viewBox="0 0 47 47"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M0 23.5009C0 10.5222 10.5213 0.000854492 23.5 0.000854492V0.000854492C36.4787 0.000854492 47 10.5222 47 23.5009V23.5009C47 36.4795 36.4787 47.0009 23.5 47.0009V47.0009C10.5213 47.0009 0 36.4795 0 23.5009V23.5009Z"
+      fill="white"
+    />
+    <path d="M34.5 7L7 24L34.5 41V7Z" fill="#57616D" />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 47,
+    },
+    height: {
+      type: Number,
+      default: 47,
+    },
+  },
+};
+</script>
---
+++ src/components/icons/ArrowRight.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg
+    :width="width"
+    :height="height"
+    viewBox="0 0 47 47"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M47 23.5C47 36.4787 36.4787 47 23.5 47V47C10.5213 47 3.89218e-05 36.4787 3.89218e-05 23.5V23.5C3.89218e-05 10.5213 10.5213 8.40425e-06 23.5 8.40425e-06V8.40425e-06C36.4787 8.40425e-06 47 10.5213 47 23.5V23.5Z"
+      fill="white"
+    />
+    <path d="M12.5 40L40 23L12.5 6L12.5 40Z" fill="#57616D" />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 47,
+    },
+    height: {
+      type: Number,
+      default: 47,
+    },
+  },
+};
+</script>
